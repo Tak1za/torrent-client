f83b4995856d2c4eaf84d3baefc00fc3245af1aa
Varun Gupta <varungupta2015135@gmail.com>
2018-07-21 19:51:46 +0530

    Added info hash and size functions to torrent-parser

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const bencode = require("bencode");
 const tracker = require("./tracker");
+const torrentParser = require("./torrent-parser");
 
-const torrent = bencode.decode(fs.readFileSync("1torr.torrent"));
+const torrent = torrentParser.open("1torr.torrent");
 
 tracker.getPeers(torrent, peers => {
   console.log("List of peers: ", peers);
