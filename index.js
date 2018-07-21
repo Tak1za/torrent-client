37ae73b153bed13e9eebc68851cb17e8ecaba9bd
Varun Gupta <varungupta2015135@gmail.com>
2018-07-21 19:12:30 +0530

    Move tracker code into a new file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const fs=require('fs');
-const torrent=fs.readFileSync('1torr.torrent');
-console.log(torrent.toString('utf8'));
+const fs = require("fs");
+const bencode = require("bencode");
+const tracker = require("./tracker");
+
+const torrent = bencode.decode(fs.readFileSync("1torr.torrent"));
+
+tracker.getPeers(torrent, peers => {
+  console.log("List of peers: ", peers);
+});
