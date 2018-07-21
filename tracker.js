37b647fcbe6ae4a6d5b9d2d9f1fbde9c6f6809e5
Varun Gupta <varungupta2015135@gmail.com>
2018-07-21 20:49:49 +0530

    MEssaging

--- tracker.js
+++ tracker.js
@@ -68,34 +68,31 @@ function buildAnnounceReq(connId, torrent, port = 6881) {
   return buf;
 }
 
-function parseAnnounceResp(resp){
-    function group(iterable, groupSize){
-        let groups=[];
-        for(let i=0; i<iterable.length, i+=groupSize){
-            groups.push(iterable.slice(i, i+groupSize));
-        }
-        return groups;
+function parseAnnounceResp(resp) {
+  function group(iterable, groupSize) {
+    let groups = [];
+    for (let i = 0; i < iterable.length; i += groupSize) {
+      groups.push(iterable.slice(i, i + groupSize));
     }
+    return groups;
+  }
 
-    return{
-        action: resp.readUInt32BE(0),
-        transactionId: resp.readUInt32BE(4),
-        leechers: resp.readUInt32BE(8),
-        seeders: resp.readUInt32BE(12),
-        peers: group(resp.slice(20), 6).map(address => {
-            return{
-                ip: address.slice(0, 4).join('.'),
-                port: address.readUInt16BE(4)
-            }
-        })
-    }
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    leechers: resp.readUInt32BE(8),
+    seeders: resp.readUInt32BE(12),
+    peers: group(resp.slice(20), 6).map(address => {
+      return {
+        ip: address.slice(0, 4).join("."),
+        port: address.readUInt16BE(4)
+      };
+    })
+  };
 }
 
-function respType(resp){
-    const action = resp.readUInt32BE(0);
-    if(action===0)
-    return 'connect';
-    if(action===1)
-    return 'announce';
+function respType(resp) {
+  const action = resp.readUInt32BE(0);
+  if (action === 0) return "connect";
+  if (action === 1) return "announce";
 }
-
