ebb288ed0c821d56d43846edafc27babc0c73f2d
Varun Gupta <varungupta2015135@gmail.com>
2018-07-21 19:54:46 +0530

    Added respType function to tracker

--- tracker.js
+++ tracker.js
@@ -91,3 +91,11 @@ function parseAnnounceResp(resp){
     }
 }
 
+function respType(resp){
+    const action = resp.readUInt32BE(0);
+    if(action===0)
+    return 'connect';
+    if(action===1)
+    return 'announce';
+}
+
